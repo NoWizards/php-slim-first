b666c3b33c4f4f61508fdabe52324c12979220d7
NoWizards <nowizards@hotmail.es>
2025-05-27 15:38:37 -0500

    add templating engine and using it, remove mustache to use twig

--- public/index.php
+++ public/index.php
@@ -1,6 +1,8 @@
 <?php
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 use Slim\Factory\AppFactory;
 //add dependency injection container
 use DI\Container;
@@ -10,11 +12,19 @@ use DI\Container;
 require __DIR__ . '/../vendor/autoload.php';
 // Create a new DI container
 $container = new Container();
-
-// Set the container to be used by the app
 AppFactory::setContainer($container);
 
+// Add any dependencies to the container here
+$container->set('view', function(){
+    return Twig::create(__DIR__ . '/../templates', [
+        'cache' => false, // Set to true in production
+    ]);
+});
+
+
+
 $app = AppFactory::create();
+$app->add(TwigMiddleware::createFromContainer($app));
 
 $app->get('/', function (Request $request, Response $response) {
     $response->getBody()->write("Hello, World!");
@@ -22,9 +32,10 @@ $app->get('/', function (Request $request, Response $response) {
 });
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = ucfirst($args['name']);
-    $response->getBody()->write(sprintf("Hello, %s!", htmlspecialchars($name)));
-    return $response;
+
+    return $this->get('view')->render($response, 'hello.html', [
+        'name' => $args['name']
+    ]);
 });
 
 $app->run();
